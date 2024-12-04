7152eed37bb8750b5cff09a97391efe5817159c5
Rambod Goshtasbi <rpg225@gmail.com>
2024-12-04 17:47:41 -0500

    setup files

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+fn main() {
+  App::new()
+  .add_plugins(DefaultPlugins.set(WindowPlugin
+    {
+        primary_window:Some(Window{
+            title:"2048".to_string(),
+            ..default()
+        }),
+        ..default()
+    }))
+  .run();
+}
